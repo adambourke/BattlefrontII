2cc33bd2c1a7ba69c6f8728a6bfe5037010a127c
Adam Bourke <adam@bourke-uk.com>
2020-03-01 12:30:46 +0100

    Change how config works

---
+++ config.js
@@ -0,0 +1,31 @@
+config = {
+    "pages":{
+        "locations":{
+            "pageType":"cardflow",
+			"dataPath": "locations"
+        },
+        "classes":{
+            "subPages":{
+                "characters":{
+                    "pageType":"cardflow",
+					"dataPath": "characters"
+                },
+                "vehicles":{
+                    "pageType":"cardflow",
+					"dataPath": "vehicles"
+                },
+                "starfighters":{
+                    "pageType":"cardflow",
+					"dataPath": "starfighters"
+                }
+            }
+        },
+        "weapons":{
+            "pageType":"cardflow",
+			"dataPath": "weapons"
+        },
+        "timeline":{
+            "pageType":"timeline"
+        }
+    }
+}
--- js/render_site.js
+++ js/render_site.js
@@ -8,18 +8,23 @@ let urlParams = new URLSearchParams(window.location.search);
 let path = urlParams.get("path");
 
 
-let fields = getFieldsFromObject(data);
+let fields = getFieldsFromObject(config.pages);
 let initialField = fields[0]
 
 if (!fields.includes(path)){
 	window.location.replace("./index.html?path=" + initialField);
 }
 
-let pagedata = data[path];
+let pageconfig = config["pages"][path];
 
 //-------Render the page!-------//
-let domContainer = document.querySelector('#page_container');
-ReactDOM.render(e(PageLayout, {data:pagedata}), domContainer);
-
+//NavigationBar
 let domContainer2 = document.querySelector('.navigationBar');
 ReactDOM.render(e(NavigationBar, {fields, selectedField:path}), domContainer2);
+
+//PageLayout
+if (pageconfig.pageType = "cardflow"){
+	pagedata = data[path]);
+	let domContainer = document.querySelector('#page_container');
+	ReactDOM.render(e(PageLayout, {data:pagedata}), domContainer);
+}
